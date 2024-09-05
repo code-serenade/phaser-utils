899912f6bd7ab3b781e83409ebe8898ae242f251
Ancient <gamesworldcraft@gmail.com>
2024-09-05 14:12:09 +0800

    fix: websocket url

--- src/services/websocket.ts
+++ src/services/websocket.ts
@@ -1,17 +1,23 @@
-import config from '../../public/config.json';
 import { protoService } from './proto_service';
 import { dataManager } from './data_manager';
 
 export class WebSocketService {
     private static instance: WebSocketService;
+    private static url: string; // 静态属性用于存储 WebSocket 的 URL
     private socket: WebSocket;
-   
 
     // 私有构造函数，防止直接通过 new 创建实例
     private constructor() {
-        const url = config.socket_url;
-        this.socket = new WebSocket(url);
+        if (!WebSocketService.url) {
+            throw new Error("WebSocket URL is not set. Use WebSocketService.setUrl() before getting the instance.");
+        }
+        this.socket = new WebSocket(WebSocketService.url);
         this.initialize();
+    }
+
+    // 静态方法设置 WebSocket URL
+    public static setUrl(url: string) {
+        WebSocketService.url = url;
     }
 
     // 获取单例实例的静态方法
@@ -30,7 +36,7 @@ export class WebSocketService {
 
         this.socket.onmessage = (event) => {
             const blob = event.data;
-        
+
             // 将 Blob 转换为 ArrayBuffer
             blob.arrayBuffer().then((buffer: ArrayBuffer) => {
                 const message = protoService.decode(buffer);
@@ -41,7 +47,6 @@ export class WebSocketService {
                 console.error("Error converting Blob to ArrayBuffer:", error);
             });
         };
-        
 
         this.socket.onclose = () => {
             console.log('Disconnected from WebSocket server');
@@ -68,4 +73,5 @@ export class WebSocketService {
     // 这里可以添加更多功能，例如事件订阅、自动重连等
 }
 
+// 获取 WebSocket 实例时不需要传递 URL
 export const socket = WebSocketService.getInstance();
