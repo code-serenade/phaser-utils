079f88f3826644de80f59757e9c532ddaaec5481
Ancient <gamesworldcraft@gmail.com>
2024-10-31 22:13:25 +0800

    fix: delete logs

--- src/services/data_manager.ts
+++ src/services/data_manager.ts
@@ -1,34 +1,33 @@
-import { eventManager } from './events';
+import { eventManager } from "./events";
 
 class DataManager {
-    private static instance: DataManager;
-    private messages: { [messageId: number]: any };
+  private static instance: DataManager;
+  private messages: { [messageId: number]: any };
 
-    private constructor() {
-        this.messages = {};
-    }
+  private constructor() {
+    this.messages = {};
+  }
 
-    public static getInstance(): DataManager {
-        if (!DataManager.instance) {
-            DataManager.instance = new DataManager();
-        }
-        return DataManager.instance;
+  public static getInstance(): DataManager {
+    if (!DataManager.instance) {
+      DataManager.instance = new DataManager();
     }
+    return DataManager.instance;
+  }
 
-    public updateMessage(messageId: number, data: any): void {
-        console.log('messageId:', messageId);
-        this.messages[messageId] = data;
-        eventManager.emit(String(messageId), data);
-    }
+  public updateMessage(messageId: number, data: any): void {
+    this.messages[messageId] = data;
+    eventManager.emit(String(messageId), data);
+  }
 
-    public getMessage(messageId: number): any | undefined {
-        return this.messages[messageId];
-    }
+  public getMessage(messageId: number): any | undefined {
+    return this.messages[messageId];
+  }
 
-    public reset(): void {
-        this.messages = {};
-        eventManager.emit('dataReset');
-    }
+  public reset(): void {
+    this.messages = {};
+    eventManager.emit("dataReset");
+  }
 }
 
 export const dataManager = DataManager.getInstance();
